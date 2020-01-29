76194ca1adc13ee39174332a17a058746bca64df
Yuniss20 <59361692+Yuniss20@users.noreply.github.com>
2020-01-29 14:22:35 +0900

    first project

---
+++ checkfee2/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace checkfee2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.depart = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.studentID = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.testfeeDataSet = new checkfee2.testfeeDataSet();
+            this.table2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table2TableAdapter = new checkfee2.testfeeDataSetTableAdapters.Table2TableAdapter();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.feeDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testfeeDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "컴퓨터공학",
+            "응용컴퓨터",
+            "소프트웨어"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 20);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // depart
+            // 
+            this.depart.AutoSize = true;
+            this.depart.Location = new System.Drawing.Point(20, 78);
+            this.depart.Name = "depart";
+            this.depart.Size = new System.Drawing.Size(29, 12);
+            this.depart.TabIndex = 19;
+            this.depart.Text = "학과";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 21);
+            this.textBox2.TabIndex = 18;
+            // 
+            // studentID
+            // 
+            this.studentID.AutoSize = true;
+            this.studentID.Location = new System.Drawing.Point(20, 51);
+            this.studentID.Name = "studentID";
+            this.studentID.Size = new System.Drawing.Size(29, 12);
+            this.studentID.TabIndex = 17;
+            this.studentID.Text = "학번";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 21);
+            this.textBox1.TabIndex = 15;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(20, 24);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(29, 12);
+            this.name.TabIndex = 14;
+            this.name.Text = "이름";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.studentIDDataGridViewTextBoxColumn,
+            this.departDataGridViewTextBoxColumn,
+            this.feeDataGridViewCheckBoxColumn});
+            this.dataGridView1.DataSource = this.table2BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(244, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(485, 209);
+            this.dataGridView1.TabIndex = 21;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 29);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(85, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 30);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "수정";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(160, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 25);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "검색";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(19, 111);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "학회비";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // testfeeDataSet
+            // 
+            this.testfeeDataSet.DataSetName = "testfeeDataSet";
+            this.testfeeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table2BindingSource
+            // 
+            this.table2BindingSource.DataMember = "Table2";
+            this.table2BindingSource.DataSource = this.testfeeDataSet;
+            // 
+            // table2TableAdapter
+            // 
+            this.table2TableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 21);
+            this.textBox3.TabIndex = 26;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.FillWeight = 80F;
+            this.nameDataGridViewTextBoxColumn.HeaderText = "이름";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 130;
+            // 
+            // studentIDDataGridViewTextBoxColumn
+            // 
+            this.studentIDDataGridViewTextBoxColumn.DataPropertyName = "studentID";
+            this.studentIDDataGridViewTextBoxColumn.HeaderText = "학번";
+            this.studentIDDataGridViewTextBoxColumn.Name = "studentIDDataGridViewTextBoxColumn";
+            this.studentIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.studentIDDataGridViewTextBoxColumn.Width = 130;
+            // 
+            // departDataGridViewTextBoxColumn
+            // 
+            this.departDataGridViewTextBoxColumn.DataPropertyName = "depart";
+            this.departDataGridViewTextBoxColumn.HeaderText = "학과";
+            this.departDataGridViewTextBoxColumn.Name = "departDataGridViewTextBoxColumn";
+            this.departDataGridViewTextBoxColumn.ReadOnly = true;
+            this.departDataGridViewTextBoxColumn.Width = 130;
+            // 
+            // feeDataGridViewCheckBoxColumn
+            // 
+            this.feeDataGridViewCheckBoxColumn.DataPropertyName = "fee";
+            this.feeDataGridViewCheckBoxColumn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.feeDataGridViewCheckBoxColumn.HeaderText = "학회비";
+            this.feeDataGridViewCheckBoxColumn.Name = "feeDataGridViewCheckBoxColumn";
+            this.feeDataGridViewCheckBoxColumn.ReadOnly = true;
+            this.feeDataGridViewCheckBoxColumn.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 247);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.depart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.studentID);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "학회비 체크";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testfeeDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label depart;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label studentID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private testfeeDataSet testfeeDataSet;
+        private System.Windows.Forms.BindingSource table2BindingSource;
+        private testfeeDataSetTableAdapters.Table2TableAdapter table2TableAdapter;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn feeDataGridViewCheckBoxColumn;
+    }
+}
+
---
+++ checkfee2/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace checkfee2
+{
+    public partial class Form1 : Form
+    {
+        
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\한윤원\Documents\testfee.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 이 코드는 데이터를 'testfeeDataSet.Table2' 테이블에 로드합니다. 필요 시 이 코드를 이동하거나 제거할 수 있습니다.
+            this.table2TableAdapter.Fill(this.testfeeDataSet.Table2);
+
+            disp_data();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 저장 기능
+        {
+     
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("insert into Table2 values('" + textBox1.Text + "','" + textBox2.Text + "','" + comboBox1.Text + "','" + checkBox1.Checked+ "')",con);
+            cmd.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("저장이 완료 되었습니다.");
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+            checkBox1.CheckState = 0;
+            disp_data();
+
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 삭제기능
+        {
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("delete from Table2 where name='" + textBox1.Text + "'", con);
+            cmd.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("삭제가 완료 되었습니다.");
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+            checkBox1.CheckState = 0;
+            disp_data();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 수정기능 *코드 재수정 해야함
+        {
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("update Table2 set name'" +textBox1.Text+"',studentID='" + textBox2.Text + "',depart='" + comboBox1.Text + "',fee='" + checkBox1.CheckState + "' where name='" + dataGridView1.SelectedRows[0].Cells[0].Value.ToString() + "')", con);
+            cmd.SelectCommand.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("수정이 완료 되었습니다.");
+            disp_data();
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //검색기능 *코드 다시 짜기 아예 재구성 해야함
+        {
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("select * from Table2 where name='" + textBox3.Text + "'", con);
+            cmd.SelectCommand.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            cmd.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            disp_data();
+
+        } 
+
+        public void disp_data() // 디스플레이 
+        {
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("select * from Table2", con);
+            cmd.SelectCommand.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            cmd.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e) 
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+        }
+    }
+}
+
+
